371d594f8c6de908082617796f87d43244a562dc
cq_home <chen_q_@163.com>
2018-01-04 22:20:15 +0800

    flow study

---
+++ my_shell/base/flow_sh.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# this is my shell flow 
+
+if [ $1 == a ]
+then 
+	echo "you input a"
+elif [ $1 == b ]
+ then 
+	echo "you input b"
+fi
---
+++ my_shell/base/fun_sh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo "this is fast $1"
+echo "this secoend $2"
+echo "this three  $4"
+echo "total $#"
+echo "all param$*"
+
+
